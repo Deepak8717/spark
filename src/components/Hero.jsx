bd495d744f4248dee871e83630c4854500e47674
Deepak8717 <sendit2deepak@gmail.com>
2022-11-17 12:55:25 +0530

    complete responsiveness of hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,19 +23,19 @@ const Hero = ({ handleToggle }) => {
     <div className="hero relative">
       <FixedHeader fixedHeader={fixedHeader} />
       <Navbar props={null} handleToggle={handleToggle} />
-      <div className="hidden">
-        <div className="absolute top-[40%] left-[20%] lg:left-[10%] xl:left-[12%] 2xl:left-[15%]   mx-auto  text-white ">
-          <h1 className="text-4xl  xl:text-5xl font-bold uppercase">
+      <div>
+        <div className="w-[90%] md:w-[80%] mt-[50px] lg:absolute top-[40%] lg:left-[10%] xl:left-[12%] 2xl:left-[15%]  mx-auto text-center lg:text-left  text-white ">
+          <h1 className="text-3xl md:text-4xl xl:text-5xl font-bold uppercase">
             corporate expense
           </h1>
-          <h2 className="text-3xl xl:text-4xl capitalize">
+          <h2 className="text-2xl md:text-3xl xl:text-4xl capitalize">
             management at your fingertips.
           </h2>
-          <p className="my-4 text-lg 2xl:text-xl w-[500px]">
+          <p className="my-4 text-md 2xl:text-xl lg:w-[500px]">
             Managing corporate expenses simplified down only to what you need.
           </p>
         </div>
-        <CorporateCard className="hidden absolute top-[70%] 2xl:top-[30%] right-[10%] lg:right-[2%] w-[80%] lg:w-[50%]" />
+        <CorporateCard className=" absolute top-[40%] md:top-[50%] lg:top-[20%] 2xl:top-[30%] right-[10%] lg:right-[2%] w-[80%] lg:w-[50%]" />
       </div>
     </div>
   );
